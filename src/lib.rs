087c87dc8223eb8bc5c0fb5f372eb22a1a44628d
Chris Latham <backwardspy@gmail.com>
2020-07-02 16:24:43 +0100

    implemented paralell decryption

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ use pyo3::prelude::*;
 use pyo3::{exceptions, wrap_pyfunction};
 
 #[pyfunction]
-fn rsa_decrypt_base64(priv_key_pem: &str, base64_text: &str) -> PyResult<String> {
-    match rustyjeff::rsa_decrypt_base64(priv_key_pem, base64_text) {
+fn rsa_decrypt_base64(priv_key_pem: &str, field_values: Vec<&str>) -> PyResult<Vec<String>> {
+    match rustyjeff::rsa_decrypt_base64(priv_key_pem, field_values) {
         Ok(result) => Ok(result),
         Err(msg) => exceptions::ValueError::into(msg),
     }
--- src/rustyjeff.rs
+++ src/rustyjeff.rs
@@ -1,26 +1,35 @@
 extern crate base64;
 extern crate openssl;
 
-use openssl::pkey::Private;
 use openssl::rsa::{Padding, Rsa};
+use rayon::prelude::*;
 
 use std::convert::TryInto;
 use std::str;
 use std::string::String;
 
-pub fn rsa_decrypt_base64(priv_key_pem: &str, base64_text: &str) -> Result<String, &'static str> {
-    let text_to_decrypt = base64::decode(base64_text).map_err(|_| "failed to decode base64")?;
-
-    let priv_key: Rsa<Private> = Rsa::private_key_from_pem(priv_key_pem.as_bytes())
+pub fn rsa_decrypt_base64(
+    priv_key_pem: &str,
+    field_values: Vec<&str>,
+) -> Result<Vec<String>, &'static str> {
+    let priv_key = Rsa::private_key_from_pem(priv_key_pem.as_bytes())
         .map_err(|_| "failed to parse private key PEM")?;
-    let data_len = std::cmp::max(text_to_decrypt.len(), priv_key.size().try_into().unwrap());
-    let mut decrypted_data: Vec<u8> = vec![0; data_len];
-    priv_key
-        .private_decrypt(&text_to_decrypt, &mut decrypted_data, Padding::PKCS1_OAEP)
-        .map_err(|_| "failed to decrypt text")?;
+    let buf_size: usize = priv_key.size().try_into().unwrap();
 
-    Ok(str::from_utf8(&decrypted_data)
-        .map_err(|_| "failed to parse UTF-8 from decrypted data")?
-        .trim_matches(char::from(0))
-        .into())
+    field_values
+        .par_iter()
+        .map(|v| {
+            let mut decrypted_data = vec![0; buf_size];
+            base64::decode(v)
+                .map_err(|_| "failed to decode base64")
+                .and_then(|value| {
+                    priv_key
+                        .private_decrypt(&value, &mut decrypted_data, Padding::PKCS1_OAEP)
+                        .map_err(|_| "failed to decrypt text")
+                        .and_then(|size| {
+                            Ok(String::from_utf8_lossy(&decrypted_data[0..size]).into_owned())
+                        })
+                })
+        })
+        .collect()
 }
